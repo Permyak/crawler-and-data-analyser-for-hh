da415b2f669e2b8ec61d4c313e4d4e29523b1e3a
Permyak <nextzucker@gmail.com>
2016-01-15 19:09:09 +0500

    Add crawler method.

--- Crawler.js
+++ Crawler.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var nconf = require('nconf');
+var nconf = require('nconf'),
+    request = require('request'),
+    cheerio = require('cheerio');
 
 //constructor
 var Crawler = function() {
@@ -8,7 +10,27 @@ var Crawler = function() {
 }
 
 Crawler.prototype.Start = function() {
-    console.log("Crawler started.");
+    var options = {
+      url: 'https://api.hh.ru/vacancies?per_page=5',
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+
+    function callback(error, response, data) {
+      if (!error && response.statusCode == 200) {
+        var elements = JSON.parse(data);
+        for (var item in elements.items) {
+          console.log(elements.items[item].id);
+        };
+      }
+      else{
+        console.log(error);
+      }
+    }
+
+    request(options, callback);
+
     return this;
 }
 
